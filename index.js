2786da845eae7d5416f75006b96e446f1b115aff
ghost-fk <fayyad_r22@yahoo.com>
2018-10-20 15:59:57 +0200

    initializing the node CLI and implement the start-up

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const files = require('./lib/files');
+
+console.log(
+  chalk.yellow(figlet.textSync('ginit', { horizontalLayout: 'full' }))
+);
