eee7158c6a7aa2eb1eb7f5bf99835c7b212894f3
ghost-fk <fayyad_r22@yahoo.com>
2018-10-20 16:22:56 +0200

    prompting the user for input i create function that will prompt the user for their github credentials

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
-
+const inquirer = require('./lib/inquirer');
 console.log(
   chalk.yellow(figlet.textSync('ginit', { horizontalLayout: 'full' }))
 );
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Aleady a git repository!'));
+  process.exit();
+}
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  console.log(credentials);
+};
+
+run();
